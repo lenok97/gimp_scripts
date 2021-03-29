a58309e2ba03837de8adb0d636214a3bf1305ea5
lenok97 <iamhappluck@gmail.com>
2021-03-30 00:41:21 +0500

    refactoring

--- deviation_measurements.py
+++ deviation_measurements.py
@@ -17,7 +17,7 @@ def write_to_file(file_name, line):
         with open(file_name, 'a') as file: 
             file.writelines(line + '\n') 
     except Exception:
-      gimp.message('Can not write results to '+ file_name + '\n Restart Gimp in admin mode and close the file in other programs')
+        gimp.message('Can not write results to '+ file_name + '\n Restart Gimp in admin mode and close the file in other programs')
 
 
 #https://github.com/VegetarianZombie/gimp-text-outliner/blob/bc4ffbdebd06e8610144767c4d28111660c5b730/text-outliner.py
@@ -97,96 +97,99 @@ def newline(x1,y1,x2,y2):
 
 
 def add_layer_into_group(image, layer_name, group, pos = 1):
-  drawable = gimp.Layer(image, layer_name, image.width, image.height, RGBA_IMAGE, 100, NORMAL_MODE)
-  pdb.gimp_image_insert_layer(image, drawable, group, pos)
-  return drawable
+    drawable = gimp.Layer(image, layer_name, image.width, image.height, RGBA_IMAGE, 100, NORMAL_MODE)
+    pdb.gimp_image_insert_layer(image, drawable, group, pos)
+    return drawable
 
 
 def add_deviation_layout(image, drawable, real_size, save_file):
-  pdb.gimp_context_push()
-  #undo-start
-  pdb.gimp_image_undo_group_start(image)
+    pdb.gimp_context_push()
+    #undo-start
+    pdb.gimp_image_undo_group_start(image)
   
   # get info from vectors
-  vectors = pdb.gimp_image_get_active_vectors(image)
-  strokes_num, strokes = pdb.gimp_vectors_get_strokes(vectors)
+    vectors = pdb.gimp_image_get_active_vectors(image)
+    strokes_num, strokes = pdb.gimp_vectors_get_strokes(vectors)
 
-  # a longer stroke is hypotenuse, other is object size mark used to find out reality/photo scale factor 
-  str0_size = pdb.gimp_vectors_stroke_get_length(vectors, strokes[0], 1)
-  str1_size = pdb.gimp_vectors_stroke_get_length(vectors, strokes[1], 1)
+    # a longer stroke is hypotenuse, other is object size mark used to find out reality/photo scale factor 
+    str0_size = pdb.gimp_vectors_stroke_get_length(vectors, strokes[0], 1)
+    str1_size = pdb.gimp_vectors_stroke_get_length(vectors, strokes[1], 1)
+    
+    if (str0_size >= str1_size):
+        hypotenuse_stroke = strokes[0]
+        photo_size = str1_size
+    else:
+        hypotenuse_stroke = strokes[1]
+        photo_size = str0_size
 
-  if (str0_size >= str1_size):
-      hypotenuse_stroke = strokes[0]
-      photo_size = str1_size
-  else:
-      hypotenuse_stroke = strokes[1]
-      photo_size = str0_size
-
-  # draw hypotenuse using coordinates of stroke points
-  stroke_type, n_points, cpoints, closed = pdb.gimp_vectors_stroke_get_points(vectors, hypotenuse_stroke)
-  c_len = len(cpoints)
-
-  start_pos = [cpoints[0], cpoints[1]]
-  end_pos = [cpoints[c_len-2], cpoints[c_len-1]]
-
-  group = pdb.gimp_layer_group_new(image)
-  pdb.gimp_item_set_name(group, 'deviation_layout')
-  pdb.gimp_image_insert_layer(image, group, None, 0)
-
-  pencil_width = int (10 / photo_size)
-  if pencil_width < 1:
-      pencil_width = 1
+    # draw hypotenuse using coordinates of stroke points
+    stroke_type, n_points, cpoints, closed = pdb.gimp_vectors_stroke_get_points(vectors, hypotenuse_stroke)
   
-  draw_pencil_lines(add_layer_into_group(image, 'hypotenuse', group), 
-                    newline(start_pos[0], start_pos[1], end_pos[0], end_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(0,255,0))
+    c_len = len(cpoints)
 
-  draw_pencil_lines(add_layer_into_group(image, 'small_leg', group), 
-                    newline(start_pos[0], end_pos[1], end_pos[0], end_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(255,0,0))
+    start_pos = [cpoints[0], cpoints[1]]
+    end_pos = [cpoints[c_len-2], cpoints[c_len-1]]
 
-  draw_pencil_lines(add_layer_into_group(image, 'small_leg_', group), 
-                    newline(start_pos[0], start_pos[1], end_pos[0], start_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(255,0,0))
+    group = pdb.gimp_layer_group_new(image)
+    pdb.gimp_item_set_name(group, 'deviation_layout')
+    pdb.gimp_image_insert_layer(image, group, None, 0)
 
-  draw_pencil_lines(add_layer_into_group(image, 'big_leg', group), 
-                    newline(start_pos[0], start_pos[1], start_pos[0], end_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(0,255,0))
-
-  draw_pencil_lines(add_layer_into_group(image, 'big_leg_', group), 
-                    newline(end_pos[0], start_pos[1], end_pos[0], end_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(0,255,0))
-
-  target_photo_size = float(abs(float(cpoints[0]) - cpoints[c_len-2]))
-  scale_factor = (real_size / float(photo_size)) #reality/photo scale factor
-  target_real_size = scale_factor * float(target_photo_size)
+    pencil_width = int (10 / photo_size)
+    if pencil_width < 1:
+        pencil_width = 1
   
-  add_text(image, str(round(target_real_size, 1)) + ' mm.', group, 
-           pos = [int(float(start_pos[0]) + end_pos[0])/2, int(float(start_pos[1]) + end_pos[1])/2],
-           points = int (0.05 * image.width))
-  img_name = pdb.gimp_image_get_filename(image)
-  
-  write_to_file(r"C:\test.csv", img_name + ';'+ str(target_real_size)+ ';mm.')
-  
-  pdb.gimp_displays_flush()
-  # undo-end
-  pdb.gimp_image_undo_group_end(image)
-  pdb.gimp_context_pop()
-  if (save_file):
-      ex = '.' + img_name.split('.')[-1]
-      out_file = img_name.replace(ex, '') + '_deviation'
-      merge_and_export(image, out_file)
-  #pdb.gimp_image_remove_vectors(image, vectors)
+    draw_pencil_lines(add_layer_into_group(image, 'hypotenuse', group), 
+                      newline(start_pos[0], start_pos[1], end_pos[0], end_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(0,255,0))
 
-def merge_and_export(image, out_file):
-  pdb.gimp_context_push()
-  #undo-start
-  pdb.gimp_image_undo_group_start(image)
-  result = pdb.gimp_image_merge_visible_layers(image, 0)
-  pdb.file_png_save(image, result, out_file+'.png', image.name, 0,9,1,1,1,1,1)
-  pdb.gimp_displays_flush()
-  pdb.gimp_image_undo_group_end(image)
-  pdb.gimp_context_pop()
+    draw_pencil_lines(add_layer_into_group(image, 'small_leg', group), 
+                      newline(start_pos[0], end_pos[1], end_pos[0], end_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(255,0,0))
+
+    draw_pencil_lines(add_layer_into_group(image, 'small_leg_', group), 
+                      newline(start_pos[0], start_pos[1], end_pos[0], start_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(255,0,0))
+
+    draw_pencil_lines(add_layer_into_group(image, 'big_leg', group), 
+                      newline(start_pos[0], start_pos[1], start_pos[0], end_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(0,255,0))
+
+    draw_pencil_lines(add_layer_into_group(image, 'big_leg_', group), 
+                      newline(end_pos[0], start_pos[1], end_pos[0], end_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(0,255,0))
+
+    target_photo_size = float(abs(float(cpoints[0]) - cpoints[c_len-2]))
+    scale_factor = (real_size / float(photo_size)) #reality/photo scale factor
+    target_real_size = scale_factor * float(target_photo_size)
+  
+    add_text(image, str(round(target_real_size, 1)) + ' mm.', group, 
+             pos = [int(float(start_pos[0]) + end_pos[0])/2, int(float(start_pos[1]) + end_pos[1])/2],
+             points = int (0.05 * image.width))
+    img_name = pdb.gimp_image_get_filename(image)
+  
+    write_to_file(r"C:\test.csv", img_name + ';'+ str(target_real_size)+ ';mm.')
+  
+    pdb.gimp_displays_flush()
+    # undo-end
+    pdb.gimp_image_undo_group_end(image)
+    pdb.gimp_context_pop()
+    if (save_file):
+        merge_and_export(image, img_name, '_deviation')
+    #pdb.gimp_image_remove_vectors(image, vectors)
+
+def merge_and_export(image, out_file, prefix = ''):
+    pdb.gimp_context_push()
+    # undo-start
+    pdb.gimp_image_undo_group_start(image)
+    # remove input file extension
+    ex = '.' + out_file.split('.')[-1] 
+    out_file = out_file.replace(ex, '')
+
+    result = pdb.gimp_image_merge_visible_layers(image, 0)
+    pdb.file_png_save(image, result, out_file + prefix + '.png', image.name, 0,9,1,1,1,1,1)
+    pdb.gimp_displays_flush()
+    pdb.gimp_image_undo_group_end(image)
+    pdb.gimp_context_pop()
 
 
 def get_angle (p1, p2):
@@ -199,62 +202,60 @@ def get_angle (p1, p2):
 
 
 def add_angle_layout(image, drawable, save_file):
-  pdb.gimp_context_push()
-  #undo-start
-  pdb.gimp_image_undo_group_start(image)
+    pdb.gimp_context_push()
+    #undo-start
+    pdb.gimp_image_undo_group_start(image)
 
-  #get info from vectors
-  vectors = pdb.gimp_image_get_active_vectors(image)
-  strokes_num, strokes = pdb.gimp_vectors_get_strokes(vectors)
-  stroke_type, n_points, cpoints, closed = pdb.gimp_vectors_stroke_get_points(vectors, strokes[0])
+    #get info from vectors
+    vectors = pdb.gimp_image_get_active_vectors(image)
+    strokes_num, strokes = pdb.gimp_vectors_get_strokes(vectors)
+    stroke_type, n_points, cpoints, closed = pdb.gimp_vectors_stroke_get_points(vectors, strokes[0])
   
-  # draw hypotenuse using coordinates of stroke points
-  c_len = len(cpoints)
-  start_pos = [cpoints[0], cpoints[1]]
-  end_pos = [cpoints[c_len-2], cpoints[c_len-1]]
+    # draw hypotenuse using coordinates of stroke points
+    c_len = len(cpoints)
+    start_pos = [cpoints[0], cpoints[1]]
+    end_pos = [cpoints[c_len-2], cpoints[c_len-1]]
 
-  pencil_width = int (1000 /image.width)
-  if pencil_width < 1:
-      pencil_width = 1
+    pencil_width = int (1000 /image.width)
+    if pencil_width < 1:
+        pencil_width = 1
 
-  group = pdb.gimp_layer_group_new(image)
-  pdb.gimp_item_set_name(group, 'angle_layout')
-  pdb.gimp_image_insert_layer(image, group, None, 0)
+    group = pdb.gimp_layer_group_new(image)
+    pdb.gimp_item_set_name(group, 'angle_layout')
+    pdb.gimp_image_insert_layer(image, group, None, 0)
 
-  draw_pencil_lines(add_layer_into_group(image, 'hypotenuse', group), 
-                    newline(start_pos[0], start_pos[1], end_pos[0], end_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(255,0,0))
-  angle = abs(90 - abs(get_angle([end_pos[0], start_pos[1]], [start_pos[0], end_pos[1]])))
-  
-  # get position to draw vertical line
-  start_line_pos = start_pos
-  end_line_pos = end_pos
-  if (end_pos[1] < start_pos[1]):
-      start_line_pos = end_pos
-      end_line_pos = start_pos
+    draw_pencil_lines(add_layer_into_group(image, 'hypotenuse', group), 
+                      newline(start_pos[0], start_pos[1], end_pos[0], end_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(255,0,0))
+    angle = abs(90 - abs(get_angle([end_pos[0], start_pos[1]], [start_pos[0], end_pos[1]])))
+    
+    # get position to draw vertical line
+    start_line_pos = start_pos
+    end_line_pos = end_pos
+    if (end_pos[1] < start_pos[1]):
+        start_line_pos = end_pos
+        end_line_pos = start_pos
 
-  draw_pencil_lines(add_layer_into_group(image, 'vertical_line', group), 
-                    newline(start_line_pos[0], start_line_pos[1], start_line_pos[0], end_line_pos[1]), 
-                    width = pencil_width, color = gimpcolor.RGB(0,255,0))
+    draw_pencil_lines(add_layer_into_group(image, 'vertical_line', group), 
+                      newline(start_line_pos[0], start_line_pos[1], start_line_pos[0], end_line_pos[1]), 
+                      width = pencil_width, color = gimpcolor.RGB(0,255,0))
 
-  add_text(image, str(round(angle, 1)) + '°', group,
-           pos = [int(float(start_pos[0]) + end_pos[0])/2, int(float(start_pos[1]) + end_pos[1])/2],
-           points = int (0.05 * image.width))
+    add_text(image, str(round(angle, 1)) + '°', group,
+             pos = [int(float(start_pos[0]) + end_pos[0])/2, int(float(start_pos[1]) + end_pos[1])/2],
+             points = int (0.05 * image.width))
  
-  img_name = pdb.gimp_image_get_filename(image)
-  write_to_file(r"C:\test.csv", img_name + ';'+ str(round(angle, 1))+ ';degrees')
+    img_name = pdb.gimp_image_get_filename(image)
+    write_to_file(r"C:\test.csv", img_name + ';'+ str(round(angle, 1))+ ';degrees')
 
-  #pdb.gimp_image_remove_vectors(image, vectors)
+    #pdb.gimp_image_remove_vectors(image, vectors)
 
-  pdb.gimp_displays_flush()
-  # undo-end
-  pdb.gimp_image_undo_group_end(image)
-  pdb.gimp_context_pop()
+    pdb.gimp_displays_flush()
+    # undo-end
+    pdb.gimp_image_undo_group_end(image)
+    pdb.gimp_context_pop()
 
-  if (save_file):
-      ex = '.'+img_name.split('.')[-1]
-      out_file = img_name.replace(ex, '') + '_angle'
-      merge_and_export(image, out_file)
+    if (save_file):
+        merge_and_export(image, img_name, '_angle')
 
 
 # Регистрируем функции в PDB
